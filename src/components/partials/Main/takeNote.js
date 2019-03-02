41fb6002fedcde583b0ae654778a9b40690339f9
baetovzamirbek <46441690+baetovzamirbek@users.noreply.github.com>
2019-03-02 15:57:13 +0600

    enter

--- src/components/partials/Main/takeNote.js
+++ src/components/partials/Main/takeNote.js
@@ -8,9 +8,8 @@ export default class TakeNote extends Component {
       <div className="col takeNoteForm">
         <div className="card">
           <span className="title"><input type="text" placeholder="Title" value={itemTitle} onChange={handleTitle} /></span>
-          <span className="takeNote"><input type="text" placeholder="Take a note" value={itemText} onChange={handleText} /></span>
+          <span className="takeNote"><textarea rows="4" cols="50" placeholder="Take a note" value={itemText} onChange={handleText}></textarea></span>
           <span className="close" onClick={handleClose}>Close</span>
-          <span className="takeNote"><textarea rows="4" cols="50" placeholder="Take a note"></textarea></span>
         </div>
       </div>
     )
